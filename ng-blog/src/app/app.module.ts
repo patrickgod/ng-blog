a7c7455277fc4061d62373dc27fd0593016190e5
Patrick God <patrick@godknows.de>
2019-03-27 20:45:52 +0100

    Added basic components to the blog.

--- ng-blog/src/app/app.module.ts
+++ ng-blog/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleListComponent } from './article-list/article-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    ArticleListComponent
   ],
   imports: [
     BrowserModule,
