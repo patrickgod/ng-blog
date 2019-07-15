968174964f17402453905485a778ba6c96a84871
Patrick God <patrick@godknows.de>
2019-07-15 06:52:55 +0200

    adding server-side rendering with angular universal

--- ng-blog/src/app/app.module.ts
+++ ng-blog/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
       NotFoundComponent
    ],
    imports: [
-      BrowserModule,
+      BrowserModule.withServerTransition({ appId: 'serverApp' }),
       AppRoutingModule
    ],
    providers: [],
